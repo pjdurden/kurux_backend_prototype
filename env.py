ce7672d8f57e34ed194d4d4f24247693b968d80b
pjdurden <prajjwalchittori1@gmail.com>
2023-05-13 22:25:29 +0530

    vercel conf

--- env.py
+++ env.py
@@ -1,2 +1,3 @@
-mongoClient = 'localhost:27017'
+# mongoClient = 'localhost:27017'
+mongoClient = 'mongodb+srv://KuruX_Test:KuruX_Test@kuruxtest.dcvfgnk.mongodb.net/?retryWrites=true&w=majority'
 dbName = 'LISTED_STOCKS'
